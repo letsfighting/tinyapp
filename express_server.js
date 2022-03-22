dce5352b560f1266d809c1af600255b785e92a86
Elroy Hui <huielroy@hotmail.com>
2022-03-22 17:19:28 -0400

    +new url form and generaterandomstring function

--- express_server.js
+++ express_server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  let index = Math.floor(Math.random() * 63);
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(index);
+  }
+
+  return result;
+}
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -30,10 +47,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
